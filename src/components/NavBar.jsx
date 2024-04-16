f881fabda6f464f0af305c094bf744660b0c7040
alexis.guillon <alexis.guillon44@orange.fr>
2024-04-16 23:10:01 +0200

    Pokedex8 repeter composant avec map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,45 @@
 import React, { useState } from 'react';
 
 function NavBar ({ pokemonIndex, setPokemonIndex, pokemonList }) {
+    console.log('%c⧭', 'color: #ff0000', pokemonList[0].name);
     
-    const Previous= () => {
-      if (pokemonIndex > 0) {
-        setPokemonIndex(pokemonIndex - 1);
-      } 
-    }
+    return (
+      pokemonList.map((pokemonIndex, el) => (
+        <button key={el} onClick={() => {
+        setPokemonIndex(el)
+        }}> {pokemonIndex.name} </button>
+
+      ))
+    );
+
+
+
+
+
+
+
+
+    // const Previous= () => {
+    //   if (pokemonIndex > 0) {
+    //     setPokemonIndex(pokemonIndex - 1);
+    //   } 
+    // }
   
   
-    const Next= () => {
-      console.log('%c⧭', 'color: #0088cc', pokemonIndex);
-      if (pokemonIndex < pokemonList.length -1) {
-        setPokemonIndex(pokemonIndex +1);
-      } else {
-        console.log('%c⧭', 'color: #000000', pokemonIndex);
-      }
-    }
-      return (
-    <div>
-      <button onClick={Previous}>Precedent</button>
-      <button onClick={Next}>Suivant</button>
-    </div>
-    )
+    // const Next= () => {
+    //   console.log('%c⧭', 'color: #0088cc', pokemonIndex);
+    //   if (pokemonIndex < pokemonList.length -1) {
+    //     setPokemonIndex(pokemonIndex +1);
+    //   } else {
+    //     console.log('%c⧭', 'color: #000000', pokemonIndex);
+    //   }
+    // }
+    //   return (
+    // <div>
+    //   <button onClick={Previous}>Precedent</button>
+    //   <button onClick={Next}>Suivant</button>
+    // </div>
+    // )
 }
 
 export default NavBar;
